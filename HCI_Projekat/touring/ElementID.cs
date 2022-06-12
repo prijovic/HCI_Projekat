426c67bbba34d42ff07b1c6a74d296710588176e
Saska Topalovic <tsaska98@gmail.com>
2022-06-12 17:35:51 +0200

    Added tutorial for adding new train and new train line

---
+++ HCI_Projekat/touring/ElementID.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCI_Projekat.touring
+{
+    class ElementID
+    {
+        public static readonly string TextBoxCode = "TextBoxCode";
+        public static readonly string TextBoxDeparturePlace = "TextBoxDeparturePlace";
+        public static readonly string TextBoxArrivalPlace = "TextBoxArrivalPlace";
+        public static readonly string BtnAddNewStation = "BtnAddNewStation";
+        public static readonly string TextBoxStation = "TextBoxStation";
+        public static readonly string BtnStation = "BtnStation";
+        public static readonly string BtnAddTrainLine = "BtnAddTrainLine";
+
+        public static readonly string TextBoxTrainCode = "TextBoxTrainCode";
+        public static readonly string TextBoxTrainName = "TextBoxTrainName";
+        public static readonly string TextBoxTrainCapacity = "TextBoxTrainCapacity";
+        public static readonly string BtnAddNewTrain = "BtnAddNewTrain";
+    }
+}
